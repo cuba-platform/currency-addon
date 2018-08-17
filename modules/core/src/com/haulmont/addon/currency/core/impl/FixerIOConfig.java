403b4e0ab81b308ba9542c948591fac0bd4bdd90
Maksim Leshukov <pacificrus63@mail.ru>
2018-08-17 14:39:56 +0400

    #2 Updates according to fixer.io external service changes

--- modules/core/src/com/haulmont/addon/currency/core/impl/FixerIOConfig.java
+++ modules/core/src/com/haulmont/addon/currency/core/impl/FixerIOConfig.java
@@ -2,11 +2,15 @@ package com.haulmont.addon.currency.core.impl;
 
 import com.haulmont.cuba.core.config.Config;
 import com.haulmont.cuba.core.config.Property;
+import com.haulmont.cuba.core.config.Source;
+import com.haulmont.cuba.core.config.SourceType;
 import com.haulmont.cuba.core.config.defaults.DefaultString;
 
+@Source(type = SourceType.DATABASE)
 public interface FixerIOConfig extends Config {
+    String REST_API_KEY_ID = "addon.currency.fixerIO.apiKey";
 
-    @Property("addon.currency.fixerIO.apiKey")
+    @Property(REST_API_KEY_ID)
     @DefaultString("")
     String getApiKey();
 }
