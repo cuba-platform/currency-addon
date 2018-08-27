217a553f85e3f7f315b84bc6d7f7287eac042f70
Maksim Leshukov <pacificrus63@mail.ru>
2018-08-27 12:31:46 +0400

    #3 Overwrite CurrencyField by addon

---
+++ modules/web/src/com/haulmont/addon/currency/web/AddonCurrencyFieldPlatformFactoryConfigurator.java
@@ -0,0 +1,18 @@
+package com.haulmont.addon.currency.web;
+
+import com.haulmont.addon.currency.web.gui.components.WebCurrencyAddonField;
+import com.haulmont.cuba.gui.components.CurrencyField;
+import com.haulmont.cuba.web.gui.WebComponentsFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class AddonCurrencyFieldPlatformFactoryConfigurator {
+
+    @PostConstruct
+    public void replaceCurrencyFieldImplementation() throws NoSuchFieldException, IllegalAccessException {
+        WebComponentsFactory.registerComponent(CurrencyField.NAME, WebCurrencyAddonField.class);
+    }
+
+}
