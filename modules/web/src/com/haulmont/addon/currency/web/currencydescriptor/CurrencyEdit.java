84110b48bc4b952e42c83662832dec95c1b74972
Maksim Leshukov <pacificrus63@mail.ru>
2018-09-21 17:09:06 +0400

    #31 Fix exception when create not popular currency

--- modules/web/src/com/haulmont/addon/currency/web/currencydescriptor/CurrencyEdit.java
+++ modules/web/src/com/haulmont/addon/currency/web/currencydescriptor/CurrencyEdit.java
@@ -4,10 +4,13 @@ import com.haulmont.addon.currency.entity.CurrencyDescriptor;
 import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.haulmont.cuba.gui.components.TextField;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Named;
 
 public class CurrencyEdit extends AbstractEditor<CurrencyDescriptor> {
+    private static final Logger LOG = LoggerFactory.getLogger(CurrencyEdit.class);
 
     @Named("fieldGroup.code")
     private TextField codeField;
@@ -15,12 +18,15 @@ public class CurrencyEdit extends AbstractEditor<CurrencyDescriptor> {
     @Override
     protected void postInit() {
         CurrencyDescriptor currency = getItem();
-        codeField.addValueChangeListener(e -> {
-            if (StringUtils.isNotBlank(currency.getCode())) {
-                java.util.Currency instance = java.util.Currency.getInstance(currency.getCode());
-                if (instance != null) {
+        String currencyCode = currency.getCode();
+        codeField.addValueChangeListener(event -> {
+            if (StringUtils.isNotBlank(currencyCode)) {
+                try {
+                    java.util.Currency instance = java.util.Currency.getInstance(currencyCode);
                     currency.setSymbol(instance.getSymbol());
                     currency.setName(instance.getDisplayName());
+                } catch (IllegalArgumentException e) {
+                    LOG.debug("Unknown currency code '{}'", currencyCode, e);
                 }
             }
         });
