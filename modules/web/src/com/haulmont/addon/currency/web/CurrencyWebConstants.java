757841160253bb1c7543689d45ae7f8a9aa9cd5d
Maksim Leshukov <pacificrus63@mail.ru>
2018-11-07 11:31:22 +0400

    #21 Implement changing side of "Change Currency" button

--- modules/web/src/com/haulmont/addon/currency/web/CurrencyWebConstants.java
+++ modules/web/src/com/haulmont/addon/currency/web/CurrencyWebConstants.java
@@ -1,13 +1,13 @@
 package com.haulmont.addon.currency.web;
 
-import com.haulmont.cuba.gui.components.CurrencyField;
+import com.haulmont.addon.currency.web.gui.components.currency_field.Side;
 
 public interface CurrencyWebConstants {
     String CURRENCY_DATE_WITH_TIME_XML_ATTR_NAME = "withTime";
     boolean CURRENCY_DATE_WITH_TIME_DEFAULT = false;
 
     String CURRENCY_BUTTON_POSITION_XML_ATTR_NAME = "currencyButtonPosition";
-    CurrencyField.CurrencyLabelPosition CURRENCY_BUTTON_POSITION_DEFAULT = CurrencyField.CurrencyLabelPosition.RIGHT;
+    Side CURRENCY_BUTTON_POSITION_DEFAULT = Side.Right;
 
     interface Css {
         String FIELD_CLASS = "c-currencyfield";
